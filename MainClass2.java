9b028a8fd31f437a2b4bbe2977623f8a82ed4fa7
Mahii09 <mohender.u09@gmail.com>
2024-07-13 21:52:32 +0530

    Add files via upload

--- MainClass2.java
+++ MainClass2.java
@@ -4,7 +4,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class MainClass {
+public class MainClass2 {
     @BeforeMethod
     public void beforeMethod() {
         long id = Thread.currentThread().getId();
@@ -28,4 +28,6 @@ public class MainClass {
         long id = Thread.currentThread().getId();
         System.out.println("After test-method. Thread id is: " + id + getClass().getSimpleName());
     }
+
+
 }
